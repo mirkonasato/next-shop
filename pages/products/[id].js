c51a6ff457be340ebb83cd6633ecaec0ec46317d
Mirko Nasato <mirko@nasato.dev>
2022-10-28 16:41:37 +0100

    Fallback: Blocking

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -8,7 +8,7 @@ export async function getStaticPaths() {
     paths: products.map((product) => ({
       params: { id: product.id.toString() },
     })),
-    fallback: false,
+    fallback: 'blocking',
   };
 }
 
