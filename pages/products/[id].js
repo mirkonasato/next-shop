5ef709d1459678b6f8d66c5cf0b345f4e4bbcacd
Mirko Nasato <mirko@nasato.dev>
2022-10-28 16:41:37 +0100

    Shared Page Component

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,6 +1,5 @@
-import Head from 'next/head';
 import Image from 'next/image';
-import Title from '../../components/Title';
+import Page from '../../components/Page';
 import { ApiError } from '../../lib/api';
 import { getProduct, getProducts } from '../../lib/products';
 
@@ -32,29 +31,23 @@ export async function getStaticProps({ params: { id } }) {
 function ProductPage({ product }) {
   console.log('[ProductPage] render:', product);
   return (
-    <>
-      <Head>
-        <title>Next Shop</title>
-      </Head>
-      <main className="px-6 py-4">
-        <Title>{product.title}</Title>
-        <div className="flex flex-col lg:flex-row">
-          <div>
-            <Image src={product.pictureUrl} alt=""
-              width={640} height={480} priority
-            />
-          </div>
-          <div className="flex-1 lg:ml-4">
-            <p className="text-sm">
-              {product.description}
-            </p>
-            <p className="text-lg font-bold mt-2">
-              {product.price}
-            </p>
-          </div>
+    <Page title={product.title}>
+      <div className="flex flex-col lg:flex-row">
+        <div>
+          <Image src={product.pictureUrl} alt=""
+            width={640} height={480} priority
+          />
         </div>
-      </main>
-    </>
+        <div className="flex-1 lg:ml-4">
+          <p className="text-sm">
+            {product.description}
+          </p>
+          <p className="text-lg font-bold mt-2">
+            {product.price}
+          </p>
+        </div>
+      </div>
+    </Page>
   );
 }
 
---
+++ pages/sign-in.js
@@ -0,0 +1,11 @@
+import Page from '../components/Page';
+
+function SignInPage() {
+  return (
+    <Page title="Sign In">
+
+    </Page>
+  );
+}
+
+export default SignInPage;
