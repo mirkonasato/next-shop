0a6339506a49d8b345f673d49d54f3d380629aa8
Mirko Nasato <mirko@nasato.dev>
2022-10-28 16:41:37 +0100

    Displaying Cart Items (step 1)

--- pages/cart.js
+++ pages/cart.js
@@ -2,6 +2,41 @@ import { useQuery } from 'react-query';
 import Page from '../components/Page';
 import { fetchJson } from '../lib/api';
 
+function CartTable({ cartItems }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th className="px-4 py-2">
+            Product
+          </th>
+          <th className="px-4 py-2">
+            Price
+          </th>
+          <th className="px-4 py-2">
+            Quantity
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {cartItems.map((cartItem) => (
+          <tr key={cartItem.id}>
+            <td className="px-4 py-2">
+              {cartItem.product.title}
+            </td>
+            <td className="px-4 py-2 text-right">
+              {cartItem.product.price}
+            </td>
+            <td className="px-4 py-2 text-right">
+              {cartItem.quantity}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
 function CartPage() {
   const query = useQuery('cartItems', () => fetchJson('/api/cart'));
   const cartItems = query.data;
@@ -9,7 +44,7 @@ function CartPage() {
   console.log('[CartPage] cartItems:', cartItems);
   return (
     <Page title="Cart">
-
+      {cartItems && <CartTable cartItems={cartItems} />}
     </Page>
   );
 }
