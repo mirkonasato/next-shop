1de598b06443191f05253d0fb5d8b0eca20ab6ca
Mirko Nasato <mirko@nasato.dev>
2022-10-28 16:41:37 +0100

    Product Page

---
+++ pages/products/[id].js
@@ -0,0 +1,38 @@
+import Head from 'next/head';
+import Title from '../../components/Title';
+import { getProduct, getProducts } from '../../lib/products';
+
+export async function getStaticPaths() {
+  const products = await getProducts();
+  return {
+    paths: products.map((product) => ({
+      params: { id: product.id.toString() },
+    })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { id } }) {
+  const product = await getProduct(id);
+  return {
+    props: { product },
+  };
+}
+
+function ProductPage({ product }) {
+  return (
+    <>
+      <Head>
+        <title>Next Shop</title>
+      </Head>
+      <main className="px-6 py-4">
+        <Title>{product.title}</Title>
+        <p>
+          {product.description}
+        </p>
+      </main>
+    </>
+  );
+}
+
+export default ProductPage;
