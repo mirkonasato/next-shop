bee6c4252176bee51c17f8fff90b6f3ffbf3f9a4
Mirko Nasato <mirko@nasato.dev>
2022-10-28 16:41:37 +0100

    Fetching Cart Items

--- pages/cart.js
+++ pages/cart.js
@@ -1,6 +1,12 @@
+import { useQuery } from 'react-query';
 import Page from '../components/Page';
+import { fetchJson } from '../lib/api';
 
 function CartPage() {
+  const query = useQuery('cartItems', () => fetchJson('/api/cart'));
+  const cartItems = query.data;
+
+  console.log('[CartPage] cartItems:', cartItems);
   return (
     <Page title="Cart">
 
