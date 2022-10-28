9ea584a94096ee8a131cc867ad0f14db2260a0de
Mirko Nasato <mirko@nasato.dev>
2022-10-28 16:41:37 +0100

    Form State and Validation

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -1,17 +1,30 @@
+import { useState } from 'react';
 import Button from '../components/Button';
 import Field from '../components/Field';
 import Input from '../components/Input';
 import Page from '../components/Page';
 
 function SignInPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('should submit:', { email, password });
+  };
+
   return (
     <Page title="Sign In">
-      <form>
+      <form onSubmit={handleSubmit}>
         <Field label="Email">
-          <Input type="email" />
+          <Input type="email" required value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
         </Field>
         <Field label="Password">
-          <Input type="password" />
+          <Input type="password" required value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
         </Field>
         <Button type="submit">
           Sign In
