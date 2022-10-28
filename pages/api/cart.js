7392a46d39e2db9cc1468c2eba4573857768aa51
Mirko Nasato <mirko@nasato.dev>
2022-10-28 16:41:37 +0100

    POST Cart Handler

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -14,7 +14,7 @@ function stripCartItem(cartItem) {
   };
 }
 
-async function handleCart(req, res) {
+async function handleGetCart(req, res) {
   const { jwt } = req.cookies;
   if (!jwt) {
     res.status(401).end();
@@ -27,6 +27,39 @@ async function handleCart(req, res) {
     res.status(200).json(cartItems.map(stripCartItem));
   } catch (err) {
     res.status(401).end();
+  }
+}
+
+async function handlePostCart(req, res) {
+  const { jwt } = req.cookies;
+  if (!jwt) {
+    res.status(401).end();
+    return;
+  }
+  const { productId, quantity } = req.body;
+  try {
+    await fetchJson(`${CMS_URL}/cart-items`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${jwt}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ product: productId, quantity }),
+    });
+    res.status(200).json({});
+  } catch (err) {
+    res.status(401).end();
+  }
+}
+
+async function handleCart(req, res) {
+  switch (req.method) {
+    case 'GET':
+      return handleGetCart(req, res);
+    case 'POST':
+      return handlePostCart(req, res);
+    default:
+      res.status(405).end();
   }
 }
 
