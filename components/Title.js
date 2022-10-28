136870d633cb3dde76477dac95dd04610247c645
Mirko Nasato <mirko@nasato.dev>
2022-10-28 16:41:37 +0100

    Utility-First CSS

---
+++ components/Title.js
@@ -0,0 +1,9 @@
+function Title({ children }) {
+  return (
+    <h1 className="text-2xl pb-4">
+      {children}
+    </h1>
+  );
+}
+
+export default Title;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Title from '../components/Title';
 
 function HomePage() {
   return (
@@ -6,8 +7,11 @@ function HomePage() {
       <Head>
         <title>Next Shop</title>
       </Head>
-      <main>
-        <h1>Next Shop</h1>
+      <main className="px-6 py-4">
+        <Title>Next Shop</Title>
+        <p>
+          [TODO: display products]
+        </p>
       </main>
     </>
   );
