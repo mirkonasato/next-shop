541d86baa677665ac2824a9a3f131a5ccf9cd97f
Mirko Nasato <mirko@nasato.dev>
2022-10-30 11:23:07 +0100

    Responsive Grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ const HomePage: React.FC<HomePageProps> = ({ products }) => {
       </Head>
       <main className="px-6 py-4">
         <Title>Next Shop</Title>
-        <ul>
+        <ul className="grid grid-cols-1 lg:grid-cols-3 gap-4">
           {products.map((product) => (
             <li key={product.id}>
               <ProductCard product={product} />
