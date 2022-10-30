6c4fc5d57f8c796d6083e391a9b8dd10b37ffe15
Mirko Nasato <mirko@nasato.dev>
2022-10-30 11:23:07 +0100

    useSignIn Custom Hook

--- hooks/user.ts
+++ hooks/user.ts
@@ -1,6 +1,39 @@
-import { useQuery } from 'react-query';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { fetchJson } from '../lib/api';
 import { User } from '../lib/user';
+
+interface SignInVariables {
+  email: string;
+  password: string;
+}
+
+interface UseSignInResult {
+  signIn: (email: string, password: string) => Promise<boolean>;
+  signInError: boolean;
+  signInLoading: boolean;
+}
+
+export function useSignIn(): UseSignInResult {
+  const queryClient = useQueryClient();
+  const mutation = useMutation<User, Error, SignInVariables>(({ email, password }) => fetchJson('/api/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email, password }),
+  }));
+  return {
+    signIn: async (email: string, password: string) => {
+      try {
+        const user = await mutation.mutateAsync({ email, password });
+        queryClient.setQueryData('user', user);
+        return true;
+      } catch (err) {
+        return false;
+      }
+    },
+    signInError: mutation.isError,
+    signInLoading: mutation.isLoading,
+  };
+}
 
 export function useUser(): User {
   const query = useQuery<User>('user', async () => {
--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -1,33 +1,22 @@
 import { useRouter } from 'next/router';
 import { FormEventHandler, useState } from 'react';
-import { useMutation, useQueryClient } from 'react-query';
 import Button from '../components/Button';
 import Field from '../components/Field';
 import Input from '../components/Input';
 import Page from '../components/Page';
-import { fetchJson } from '../lib/api';
-import { User } from '../lib/user';
+import { useSignIn } from '../hooks/user';
 
 const SignInPage: React.FC = () => {
   const router = useRouter();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const queryClient = useQueryClient();
-  const mutation = useMutation<User>(() => fetchJson('/api/login', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ email, password }),
-  }));
+  const { signIn, signInError, signInLoading } = useSignIn();
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
     event.preventDefault();
-    try {
-      const user = await mutation.mutateAsync();
-      queryClient.setQueryData('user', user);
-      console.log('signed in:', user);
+    const valid = await signIn(email, password);
+    if (valid) {
       router.push('/');
-    } catch (err) {
-      // mutation.isError will be true
     }
   };
 
@@ -44,12 +33,12 @@ const SignInPage: React.FC = () => {
             onChange={(event) => setPassword(event.target.value)}
           />
         </Field>
-        {mutation.isError && (
+        {signInError && (
           <p className="text-red-700">
             Invalid credentials
           </p>
         )}
-        {mutation.isLoading ? (
+        {signInLoading ? (
           <p>Loading...</p>
         ) : (
           <Button type="submit">
