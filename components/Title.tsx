4b5101715946d6ed858f7d7c6c47b0519a72e4b4
Mirko Nasato <mirko@nasato.dev>
2022-10-30 11:23:07 +0100

    Utility-First CSS

---
+++ components/Title.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from 'react';
+
+const Title: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <h1 className="text-2xl pb-4">
+      {children}
+    </h1>
+  );
+};
+
+export default Title;
