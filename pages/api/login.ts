3e89b355b00ff095792eee85e01e99b434b61918
Mirko Nasato <mirko@nasato.dev>
2022-10-30 11:23:07 +0100

    API Route with POST

---
+++ pages/api/login.ts
@@ -0,0 +1,12 @@
+import { NextApiHandler } from 'next';
+
+const handleLogin: NextApiHandler = (req, res) => {
+  if (req.method !== 'POST') {
+    res.status(405).end();
+    return;
+  }
+  console.log('req.body:', req.body);
+  res.status(200).json({});
+};
+
+export default handleLogin;
