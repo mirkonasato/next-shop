d48b350c0ceea40b649f63056bda30d3b6e21997
Mirko Nasato <mirko@nasato.dev>
2022-10-30 11:23:07 +0100

    Product Page

--- lib/products.ts
+++ lib/products.ts
@@ -1,13 +1,21 @@
 export interface Product {
   id: number;
   title: string;
+  description: string;
 }
 
 function stripProduct(product: any): Product {
   return {
     id: product.id,
     title: product.title,
+    description: product.description,
   };
+}
+
+export async function getProduct(id: string): Promise<Product> {
+  const response = await fetch(`http://localhost:1337/products/${id}`);
+  const product = await response.json();
+  return stripProduct(product);
 }
 
 export async function getProducts(): Promise<Product[]> {
