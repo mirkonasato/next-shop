2117f506c6f5dd9e087741a3cecafa8177aa7409
Mirko Nasato <mirko@nasato.dev>
2022-10-30 11:23:07 +0100

    Displaying Cart Items (step 1)

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -3,6 +3,45 @@ import Page from '../components/Page';
 import { fetchJson } from '../lib/api';
 import { CartItem } from '../lib/cart';
 
+interface CartTableProps {
+  cartItems: CartItem[];
+}
+
+const CartTable: React.FC<CartTableProps> = ({ cartItems }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th className="px-4 py-2">
+            Product
+          </th>
+          <th className="px-4 py-2">
+            Price
+          </th>
+          <th className="px-4 py-2">
+            Quantity
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {cartItems.map((cartItem) => (
+          <tr key={cartItem.id}>
+            <td className="px-4 py-2">
+              {cartItem.product.title}
+            </td>
+            <td className="px-4 py-2 text-right">
+              {cartItem.product.price}
+            </td>
+            <td className="px-4 py-2 text-right">
+              {cartItem.quantity}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
 const CartPage: React.FC = () => {
   const query = useQuery<CartItem[]>('cartItems', () => fetchJson('/api/cart'));
   const cartItems = query.data;
@@ -10,7 +49,7 @@ const CartPage: React.FC = () => {
   console.log('[CartPage] cartItems:', cartItems);
   return (
     <Page title="Cart">
-
+      {cartItems && <CartTable cartItems={cartItems} />}
     </Page>
   );
 };
