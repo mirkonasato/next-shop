bb877c77133e74debc5802c964f2a39a34f7a6db
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Next Image Component

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,10 +1,11 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 function ProductCard({ product }) {
   return (
     <div className="border w-80 shadow hover:shadow-xl">
       <Link href={`/products/${product.id}`}>
-        <img src={product.pictureUrl} alt="" />
+        <Image src={product.pictureUrl} alt="" width={320} height={240} />
         <div className="p-2 flex justify-between items-baseline">
           <h2 className="text-lg font-bold">
             {product.title}
