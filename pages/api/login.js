de71b58e17ad8503d809b49fc92a5d6116ecdcd8
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Login API Route

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,10 +1,25 @@
-function handleLogin(req, res) {
+import { fetchJson } from '../../lib/api';
+
+async function handleLogin(req, res) {
   if (req.method !== 'POST') {
     res.status(405).end();
     return;
   }
-  console.log('req.body:', req.body);
-  res.status(200).json({});
+  const { email, password } = req.body;
+  try {
+    const { jwt, user } = await fetchJson('http://localhost:1337/auth/local', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ identifier: email, password }),
+    });
+    //TODO set jwt cookie
+    res.status(200).json({
+      id: user.id,
+      name: user.username,
+    });
+  } catch (err) {
+    res.status(401).end();
+  }
 }
 
 export default handleLogin;
