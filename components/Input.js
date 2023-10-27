6dd576d9fd23f846210567d55bf614ba4da7d592
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Form State and Validation

--- components/Input.js
+++ components/Input.js
@@ -1,6 +1,7 @@
-function Input({ type }) {
+function Input({ type, required, value, onChange }) {
   return (
-    <input type={type}
+    <input type={type} required={required}
+      value={value} onChange={onChange}
       className="border rounded px-3 py-1 w-80"
     />
   );
