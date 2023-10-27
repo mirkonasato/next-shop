c505fbe249e2a2e15ff0c3f71f1524d290c6c8ff
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Why React Query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import '../styles/globals.css';
+
+const queryClient = new QueryClient();
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <Component {...pageProps} />
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
   );
 };
 
