fdfe5eb9e03bfe0cc37bd4bb7636f620c42ef6b7
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    NavBar with Authentication

---
+++ components/NavBar.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+
+const NavBar: React.FC = () => {
+  const user = undefined;
+  return (
+    <nav className="px-2 py-1 text-sm">
+      <ul className="flex gap-2">
+        <li className="text-lg font-extrabold">
+          <Link href="/">
+            Next Shop
+          </Link>
+        </li>
+        <li role="separator" className="flex-1" />
+        {user ? (
+          <>
+            <li>
+              {user.name}
+            </li>
+            <li>
+              <button>Sign Out</button>
+            </li>
+          </>
+        ) : (
+          <li>
+            <Link href="/sign-in">
+              Sign In
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
--- components/Page.tsx
+++ components/Page.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import { PropsWithChildren } from 'react';
+import NavBar from './NavBar';
 import Title from './Title';
 
 interface PageProps extends PropsWithChildren {
@@ -12,6 +13,9 @@ const Page: React.FC<PageProps> = ({ title, children }) => {
       <Head>
         <title>{`${title} - Next Shop`}</title>
       </Head>
+      <header>
+        <NavBar />
+      </header>
       <main className="px-6 py-4">
         <Title>{title}</Title>
         {children}
