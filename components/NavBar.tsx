4b80c82b039388c9585782feee3695e16a5c6d96
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    useQuery Hook

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,24 +1,24 @@
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 import { fetchJson } from '../lib/api';
 import { User } from '../lib/user';
 
 const NavBar: React.FC = () => {
-  const [user, setUser] = useState<User>();
-  useEffect(() => {
-    (async () => {
-      try {
-        const user = await fetchJson('/api/user');
-        setUser(user);
-      } catch (err) {
-        // not signed in
-      }
-    })();
-  }, []);
+  const query = useQuery<User>('user', async () => {
+    try {
+      return await fetchJson('/api/user');
+    } catch (err) {
+      return undefined;
+    }
+  }, {
+    cacheTime: Infinity,
+    staleTime: 30_000, // ms
+  });
+  const user = query.data;
 
   const handleSignOut = async () => {
     await fetchJson('/api/logout');
-    setUser(undefined);
+    //FIXME setUser(undefined);
   };
 
   console.log('[NavBar] user:', user);
