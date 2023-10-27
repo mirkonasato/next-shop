09c131acc9ce1dd9e525ad131c1e493ff1e7f07d
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Create Next App

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-import '@/styles/globals.css'
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+    <Component {...pageProps} />
+  );
 }
+
+export default App;
