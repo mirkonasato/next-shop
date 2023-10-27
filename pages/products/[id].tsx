af4e32ae701c0890c2cb91154010036d21f079ac
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Fallback: Blocking

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -18,7 +18,7 @@ export const getStaticPaths: GetStaticPaths<ProductPageParams> = async () => {
     paths: products.map((product) => ({
       params: { id: product.id.toString() },
     })),
-    fallback: false,
+    fallback: 'blocking',
   };
 };
 
