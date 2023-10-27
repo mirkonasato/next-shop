812d18532bff69af830cc83c17f80e5c1128f335
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Custom Error Class

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,17 @@
+export class ApiError extends Error {
+  constructor(url: string, public status: number) {
+    super(`'${url}' returned ${status}`);
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ApiError);
+    }
+    this.name = 'ApiError';
+  }
+}
+
 export async function fetchJson(url: string): Promise<any> {
   const response = await fetch(url);
   if (!response.ok) {
-    throw new Error(`request failed: ${response.status}`);
+    throw new ApiError(url, response.status);
   }
   return await response.json();
 }
