43c62908660e8563e481d10a60d297fbe93f932e
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Sign In API Request

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -3,14 +3,20 @@ import Button from '../components/Button';
 import Field from '../components/Field';
 import Input from '../components/Input';
 import Page from '../components/Page';
+import { fetchJson } from '../lib/api';
 
 function SignInPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log('should submit:', { email, password });
+    const response = await fetchJson('http://localhost:1337/auth/local', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ identifier: email, password }),
+    });
+    console.log('sign in:', response);
   };
 
   return (
