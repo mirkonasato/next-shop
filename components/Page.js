8ba6f7759c7f177c96a180ff446f8aef71e60bb3
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    NavBar with Authentication

--- components/Page.js
+++ components/Page.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import NavBar from './NavBar';
 import Title from './Title';
 
 function Page({ title, children }) {
@@ -7,6 +8,9 @@ function Page({ title, children }) {
       <Head>
         <title>{`${title} - Next Shop`}</title>
       </Head>
+      <header>
+        <NavBar />
+      </header>
       <main className="px-6 py-4">
         <Title>{title}</Title>
         {children}
