b71e442a8226cd1f29367aa5251bb330493ca36f
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Next Image Component

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import { Product } from '../lib/products';
 
@@ -9,7 +10,9 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   return (
     <div className="border w-80 shadow hover:shadow-xl">
       <Link href={`/products/${product.id}`}>
-        <img src={product.pictureUrl} alt="" />
+        <Image src={product.pictureUrl} alt=""
+          width={320} height={240}
+        />
         <div className="p-2 flex justify-between items-baseline">
           <h2 className="text-lg font-bold">
             {product.title}
